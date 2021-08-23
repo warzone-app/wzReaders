695c963b2deb3c8e465514bd6a15c1bd966ffc17
James Yon <jamesyon55@gmail.com>
2021-08-23 13:40:40 -0500

    React: Added new components

---
+++ src/components/Graph.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Graph extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is where the graphs will go</h1>
+      </div>
+    );
+  }
+}
+
+export default Graph;
---
+++ src/components/MatchHistory.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class MatchHistory extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is where the match history will go</h1>
+      </div>
+    );
+  }
+}
+
+export default MatchHistory;

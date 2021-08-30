c749318fd1d1476210816d4cc97d217a5ea05211
James Yon <jamesyon55@gmail.com>
2021-08-30 16:16:14 -0500

    Get match Id

--- src/components/MatchHistory.js
+++ src/components/MatchHistory.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import "./styles/MatchHistory.css";
 
 class MatchHistory extends Component {
   render() {
+    console.log(123, this.props.matchId.data.matches);
     return (
       <div id="matchContainer">
         <div className="singleMatch">
@@ -37,4 +39,10 @@ class MatchHistory extends Component {
   }
 }
 
-export default MatchHistory;
+const mapState = (state) => {
+  return {
+    matchId: state.landingPage.matchId,
+  };
+};
+
+export default connect(mapState, null)(MatchHistory);
